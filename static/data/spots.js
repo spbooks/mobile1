4cd09cadade24c7b90e88699bad1e05e8d95944f
Mr Speaker <mrspeaker@gmail.com>
2011-03-28 00:41:43 +0200

    code for ch4

---
+++ static/data/spots.js
@@ -0,0 +1,16 @@
+[{
+  "id": 4,
+  "name": "Planet Bollywood", 
+  "distance": "400m", 
+  "sightings": 10
+}, {
+  "id": 7,
+  "name": "Soft Rock Café", 
+  "distance": "1.1Km", 
+  "sightings": 3
+}, {
+  "id": 21,
+  "name": "Studio 666", 
+  "distance": "1.3Km", 
+  "sightings": 8
+}]
